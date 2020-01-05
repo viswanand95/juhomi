035beaebabcb7fca616bf0af6b54dec8ee10a633
viswanand95 <viswanand95@gmail.com>
2020-01-05 23:27:02 +0530

    Add files via upload

---
+++ ex1 Introduction.rb
@@ -0,0 +1,4 @@
+puts "Hello"
+puts "Hello again"
+puts "I am anand"
+puts "How are you"
---
+++ ex2 comment.rb
@@ -0,0 +1,5 @@
+puts "Hello"
+puts "Hello again" # this is comment
+# we can use anywhere we want
+puts "I am anand"
+puts "How are you"
